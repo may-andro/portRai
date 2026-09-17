704b6e5c29cfdf09614209d5d213c40f7670c952
may-andro <mayank271993@gmail.com>
2026-09-17 16:53:43 +0200

    fixes the analyzer failure

--- layer/firebase/lib/src/crashlytics/fb_crashlytics_controller.dart
+++ layer/firebase/lib/src/crashlytics/fb_crashlytics_controller.dart
@@ -13,13 +13,13 @@ class FbCrashlyticsController {
 
   Future<void> setCrashlyticsEnabled(bool isEnabled) async {
     if (_isFeatureSupported) {
-      return _firebaseCrashlytics.setCrashlyticsCollectionEnabled(isEnabled);
+      return await _firebaseCrashlytics.setCrashlyticsCollectionEnabled(isEnabled);
     }
   }
 
   Future<void> setUser(String userId) async {
     if (_isOperationAllowed) {
-      return _firebaseCrashlytics.setUserIdentifier(userId);
+      return await _firebaseCrashlytics.setUserIdentifier(userId);
     }
   }
 
@@ -31,7 +31,7 @@ class FbCrashlyticsController {
 
   Future<void> logMessage(String message) async {
     if (_isOperationAllowed) {
-      return _firebaseCrashlytics.log(message);
+      return await _firebaseCrashlytics.log(message);
     }
   }
 
@@ -41,7 +41,7 @@ class FbCrashlyticsController {
     bool isFatal = false,
   }) async {
     if (_isOperationAllowed) {
-      return _firebaseCrashlytics.recordError(
+      return await _firebaseCrashlytics.recordError(
         error,
         stackTrace,
         fatal: isFatal,
@@ -51,7 +51,7 @@ class FbCrashlyticsController {
 
   Future<void> reportFlutterError(FlutterErrorDetails error) async {
     if (_isOperationAllowed) {
-      return _firebaseCrashlytics.recordFlutterError(error);
+      return await _firebaseCrashlytics.recordFlutterError(error);
     }
   }
 
